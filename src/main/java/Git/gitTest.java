634c4e89faf041b173356660313515325e9e8124
luting <luting1723250470@qq.com>
2021-06-08 18:15:07 +0800

    master commit2

--- src/main/java/Git/gitTest.java
+++ src/main/java/Git/gitTest.java
@@ -11,5 +11,6 @@ public class gitTest {
         System.out.println("hello,git");
         System.out.println("hello,git");
         System.out.println("hello,git4");
+        System.out.println("master test!");
     }
 }
